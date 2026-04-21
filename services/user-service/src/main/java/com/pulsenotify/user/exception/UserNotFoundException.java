e92aa66c96dc6e5783ed0c430d9726d9ffe7c1de
SebaAre <arellanosebastian5@hotmail.com>
2026-04-21 11:18:45 -0300

    feat: added UserNotFoundException for user-service

---
+++ services/user-service/src/main/java/com/pulsenotify/user/exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.pulsenotify.user.exception;
+
+import java.util.UUID;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(UUID id){
+        super("User not found with id: " + id);
+    }
+
+    public UserNotFoundException(String email){
+        super("User not found with email: " + email);
+    }
+    
+}
