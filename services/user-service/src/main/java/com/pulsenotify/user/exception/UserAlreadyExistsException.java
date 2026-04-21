c55c838e87318e3a17d73ce58718e255219fa4cd
SebaAre <arellanosebastian5@hotmail.com>
2026-04-21 11:22:20 -0300

    feat: added UserAlreadyExistsException for user-service

---
+++ services/user-service/src/main/java/com/pulsenotify/user/exception/UserAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.pulsenotify.user.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String email) {
+        super ("User already exists with email: " + email);
+    }
+
+}
