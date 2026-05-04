d8afb4eb807f15d62748fccb58b95dca72e6b565
SebaAre <arellanosebastian5@hotmail.com>
2026-05-04 13:11:55 -0300

    feat: added RateLimitConfig in gateway-service

---
+++ services/gateway-service/src/main/java/com/pulsenotify/gateway/config/RateLimitConfig.java
@@ -0,0 +1,19 @@
+package com.pulsenotify.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> {
+            var addr = exchange.getRequest().getRemoteAddress();
+            String key = addr != null ? addr.getAddress().getHostAddress() : "unknown";
+            return Mono.just(key);
+        };
+    }
+}
